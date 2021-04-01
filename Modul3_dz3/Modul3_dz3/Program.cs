7b487ab76465a5f8e25cb5cbd59208633b1c1a0c
denys-u <denys.udovik@nure.ua>
2021-04-01 23:28:44 +0300

    created new project

---
+++ Modul3_dz3/Modul3_dz3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Modul3_dz3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
