6ea0c121fdf6bde1bfbed0de3c9c2cbb0e577be2
denys-u <denys.udovik@nure.ua>
2021-04-06 21:05:46 +0300

    edit

--- Modul3_dz3/Modul3_dz3/Program.cs
+++ Modul3_dz3/Modul3_dz3/Program.cs
@@ -1,12 +1,29 @@
-﻿using System;
-
-namespace Modul3_dz3
+﻿namespace Modul3_dz3
 {
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Threading.Tasks;
+
     public class Program
     {
-        static void Main(string[] args)
+        private const string Path = "text.txt";
+
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var result = MethodAsync().GetAwaiter().GetResult();
+            Console.WriteLine(result);
+        }
+
+        public static async Task<string> MethodAsync()
+        {
+            var list = new List<Task<string>>();
+
+            list.Add(Task.Run(() => "World"));
+            list.Add(Task.Run(() => File.ReadAllTextAsync(Path)));
+
+            var result = await Task.WhenAll(list);
+            return string.Join(" ", result);
         }
     }
 }
